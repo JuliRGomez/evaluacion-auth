c64445d7daca504ebdab1c005c2d1124e0e9d9dc
juli <julirg58@gmail.com>
2021-02-08 14:33:15 -0500

    ingresar usuario

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -12,6 +12,48 @@ export const login = async (req, res) => {
 // - responder con un codigo de estado fallido 400 > cuando hagan falta campos o cuando el usuario ya exista en la base de datos
 // - responder con el objeto del usuario que ha sido creado y un codigo 201 cuando el registro sea satisfactorio
 export const signIn = async (req, res) => {
+    const data= req.body;
+    try{
+        if(check(data)){  
+            const result= await Users.findOne({
+                where:{
+                    email:data.email
+                }
+            })
+            if(result){
+                res.status(400).send();
+            }
+            else{
+                console.log("creando");
+                //const r = await Users.create()
+                
+            }
+        }
+        else{
+        return res.status(400).send();
+        }
+
+       
+
+    }
+    catch(error){
+        console.log(error);
+    }
+   
+
     
+}
+
+const check = (data) =>{
+    
+    if(data.firstName&&data.lastName&&data.email&&data.password){
+        return true;
+    }
+       
+    else{
+        return false;
+    }
+        
+        
 }
 
